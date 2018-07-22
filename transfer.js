b73e3f18985657523fc78d6f808ca14b8f584434
Your Name <you@example.com>
2018-07-22 11:24:16 +0000

    add trasfer file

---
+++ transfer.js
@@ -0,0 +1,22 @@
+const Sunbeam = require('sunbeam')
+const Eos = require('eosjs')
+
+keyProvider= '5Jsoz5kcmKLyYwNgud3C7UHKs3RHi7b4izPpjC9n6yNT8VyUqbc'
+httpEndpoint=   'http://35.189.86.89:8888'
+chainId=   'cf057bbfb72640471fd910bcb67639c22df9f92470936cddc1ade0e2f2e7dc4f' 
+
+// Localhost Testnet (run ./docker/up.sh)
+// Connect to a testnet or mainnet
+eos = Eos({httpEndpoint,  keyProvider})
+
+      eos.contract('efinextether').then((contract) => {
+      const args = {
+      }
+      return contract.transfer({ "from": "testuser1122", "to": "efinexchange", "quantity": "10.0000000000 BTC", "memo": "go time"})
+     //return contract.cancel(args, this.getAuth())
+     })
+      .then((res) => {
+        console.log(res)
+      })
+     .catch((err) => { console.log(err) })
+
